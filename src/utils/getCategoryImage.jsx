9110e5cbee491f22a0e312cf822e6b6b7353f447
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2024-10-03 23:50:49 +0545

    Update getCategoryImage.jsx

--- src/utils/getCategoryImage.jsx
+++ src/utils/getCategoryImage.jsx
@@ -1,12 +1,12 @@
 const categoryImages = {
-    "Religious Temple": "https://via.placeholder.com/300x200?text=Religious+Temple",
-    "Religious": "https://via.placeholder.com/300x200?text=Religious",
-    "Religious cave": "https://via.placeholder.com/300x200?text=Religious+Cave",
-    "Religious Site": "https://via.placeholder.com/300x200?text=Religious+Site",
+    "Religious Temple": "https://dummyimage.com/300x200?text=Religious+Temple",
+    "Religious": "https://dummyimage.com/300x200?text=Religious",
+    "Religious cave": "https://dummyimage.com/300x200?text=Religious+Cave",
+    "Religious Site": "https://dummyimage.com/300x200?text=Religious+Site",
   };
   
   const getCategoryImage = (category) => {
-    return categoryImages[category] || "https://via.placeholder.com/300x200?text=Default"; 
+    return categoryImages[category] || "https://dummyimage.com/300x200?text=Default"; 
   };
   
   export default getCategoryImage;
